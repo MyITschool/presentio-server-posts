1f9819f95a917c834f53f4f19a954ab8d44d4f0b
Evonicgu <evonicgu@gmail.com>
2022-05-12 01:43:24 +0300

    Updated gorm annotations

--- src/v0/models/post.go
+++ src/v0/models/post.go
@@ -4,8 +4,8 @@ import "time"
 
 type Post struct {
 	ID        int64     `gorm:"primaryKey" json:"id"`
-	User      User      `json:"user" binding:"required"`
 	UserID    int64     `binding:"required"`
+	User      User      `gorm:"foreignKey:UserID;references:id;" json:"user" binding:"required"`
 	Text      string    `json:"text" binding:"required"`
 	CreatedAt time.Time `json:"createdAt" binding:"required" gorm:"type:timestamp"`
 }
