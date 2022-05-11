11d07bf865ae8e5fa6518efb8a3eccf399643afc
Evonicgu <evonicgu@gmail.com>
2022-05-12 01:38:12 +0300

    Changed field names to have no-configuration foreign keys available

--- src/v0/models/post.go
+++ src/v0/models/post.go
@@ -4,8 +4,8 @@ import "time"
 
 type Post struct {
 	ID        int64     `gorm:"primaryKey" json:"id"`
-	User      User      `gorm:"foreignKey:user_id" json:"user" binding:"required"`
-	UserId    int64     `binding:"required"`
+	User      User      `json:"user" binding:"required"`
+	UserID    int64     `binding:"required"`
 	Text      string    `json:"text" binding:"required"`
 	CreatedAt time.Time `json:"createdAt" binding:"required" gorm:"type:timestamp"`
 }
