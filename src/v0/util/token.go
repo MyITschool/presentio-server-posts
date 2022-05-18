96579bdbbefcfae4c9d1c9ad484eeca81d4f1bce
Evonicgu <evonicgu@gmail.com>
2022-05-19 01:55:12 +0300

    Changed token from debug to prod

--- src/v0/util/token.go
+++ src/v0/util/token.go
@@ -20,15 +20,15 @@ func init() {
 	var err error
 
 	accessValidateKey, err = jwt.ParseRSAPublicKeyFromPEM([]byte(os.Getenv("TOKEN_PUBLIC_KEY")))
-
-	if err != nil {
-		var token []byte
-		token, err = os.ReadFile("token")
-
-		if err == nil {
-			accessValidateKey, err = jwt.ParseRSAPublicKeyFromPEM(token)
-		}
-	}
+	//
+	//if err != nil {
+	//	var token []byte
+	//	token, err = os.ReadFile("token")
+	//
+	//	if err == nil {
+	//		accessValidateKey, err = jwt.ParseRSAPublicKeyFromPEM(token)
+	//	}
+	//}
 
 	if err != nil {
 		panic("Unable to read RSA public key")
